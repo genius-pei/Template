93d254dd640e2834dafe67b3c08a28497f565b24
Yiming <2018565194@qq.com>
2025-08-06 22:20:09 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -16,23 +16,39 @@ using namespace std;
 //	stack<int> s2;//1000,单单使用宏N无法满足创建不同容量栈的需求
 //}
 //使用非类型模版参数
-template<class T=int,size_t N=100>
-class stack2
+//template<class T=int,size_t N=100>
+//class stack2
+//{
+//private:
+//	T _a[N];
+//	int _top;
+//	int _capacity;
+//};
+//int main()
+//{
+//	stack2<int,10> s1;//10
+//	stack2<int,1000> s2;//1000
+//	stack2<int> s3;
+//	stack2<> s4;
+//	cout << sizeof(s1) << endl;
+//	cout << sizeof(s2) << endl;
+//	cout << sizeof(s3) << endl;
+//	cout << sizeof(s4) << endl;
+//	return 0; 
+//}
+template<class T>
+bool Less(T left, T right)
 {
-private:
-	T _a[N];
-	int _top;
-	int _capacity;
-};
+	return left < right;
+}
+//特化
+template<>
+bool Less<double*>(double* left,double* right)
+{
+	return *left < *right;
+}
 int main()
 {
-	stack2<int,10> s1;//10
-	stack2<int,1000> s2;//1000
-	stack2<int> s3;
-	stack2<> s4;
-	cout << sizeof(s1) << endl;
-	cout << sizeof(s2) << endl;
-	cout << sizeof(s3) << endl;
-	cout << sizeof(s4) << endl;
-	return 0; 
+	cout << Less(3, 5) << endl;
+	return 0;
 }
