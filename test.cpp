d53f6743c54d842f72aa0b2d8e4181cfdf8bc75e
Yiming <2018565194@qq.com>
2025-08-08 23:12:38 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include"FUNC.h"
 using namespace std;
 //#define N 10
 //template<class T>
@@ -36,92 +37,97 @@ using namespace std;
 //	cout << sizeof(s4) << endl;
 //	return 0; 
 //}
-template<class T>
-bool Less(const T& left, const T& right)//使用&避免传值传参，同时const修饰避免使用引用将原值修改
-{
-	return left < right;
-}
-//特化   
-template<>
-bool Less<double*>(double* const & left,double* const & right)//const 在*左边修饰指针指向对象，在*  w右边修饰指针本身
-{
-	return *left < *right;	
-}
-template<>
-bool Less<string*>(string* const & left, string* const & right)
-{
-	return *left < *right;
-}
-
-
-template<class T1,class T2>
-class Date
-{
-public:
-	Date()
-	{
-		cout << "Date(T1,T2)" << endl;
-	}
-private:
-	T1 d1;
-	T2 d2;
-
-};
-//全特化
-template<>
-class Date<int,char>
-{
-public:
-	Date()
-	{
-		cout << "Date(int,char)" << endl;
-	}
-};
-//偏特化
-//特化部分参数
-template<class T1>
-class Date<T1,char>
-{
-public:
-	Date()
-	{
-		cout << "Date(T1,char)" << endl;
-	}
-};
-//偏特化
-//对参数进一步限制
-template<class T1,class T2>
-class Date<T1*, T2*>
-{
-public:
-	Date()
-	{
-		cout << "Date(T1*,T2*)" << endl;
-	}
-};
-
-template<class T1, class T2>
-class Date<T1&, T2&>
-{
-public:
-	Date()
-	{
-		cout << "Date(T1&,T2&)" << endl;
-	}
-};
-
-
+//template<class T>
+//bool Less(const T& left, const T& right)//使用&避免传值传参，同时const修饰避免使用引用将原值修改
+//{
+//	return left < right;
+//}
+////特化   
+//template<>
+//bool Less<double*>(double* const & left,double* const & right)//const 在*左边修饰指针指向对象，在*  w右边修饰指针本身
+//{
+//	return *left < *right;	
+//}
+//template<>
+//bool Less<string*>(string* const & left, string* const & right)
+//{
+//	return *left < *right;
+//}
+//
+//
+//template<class T1,class T2>
+//class Date
+//{
+//public:
+//	Date()
+//	{
+//		cout << "Date(T1,T2)" << endl;
+//	}
+//private:
+//	T1 d1;
+//	T2 d2;
+//
+//};
+////全特化
+//template<>
+//class Date<int,char>
+//{
+//public:
+//	Date()
+//	{
+//		cout << "Date(int,char)" << endl;
+//	}
+//};
+////偏特化
+////特化部分参数
+//template<class T1>
+//class Date<T1,char>
+//{
+//public:
+//	Date()
+//	{
+//		cout << "Date(T1,char)" << endl;
+//	}
+//};
+////偏特化
+////对参数进一步限制
+//template<class T1,class T2>
+//class Date<T1*, T2*>
+//{
+//public:
+//	Date()
+//	{
+//		cout << "Date(T1*,T2*)" << endl;
+//	}
+//};
+//
+//template<class T1, class T2>
+//class Date<T1&, T2&>
+//{
+//public:
+//	Date()
+//	{
+//		cout << "Date(T1&,T2&)" << endl;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	/*cout << Less(3, 5) << endl;
+//	cout << Less(3.3, 2.2) << endl;
+//	cout << Less('ABC', 'AB') << endl;*/
+//	Date<int, int> d1;
+//	Date<int, char> d2;
+//	Date<char, char> d3;
+//	Date<int*, char*> d4;
+//	Date<int&, char&> d5;   
+//		
+//	
+//	return 0;
+//}
 int main()
 {
-	/*cout << Less(3, 5) << endl;
-	cout << Less(3.3, 2.2) << endl;
-	cout << Less('ABC', 'AB') << endl;*/
-	Date<int, int> d1;
-	Date<int, char> d2;
-	Date<char, char> d3;
-	Date<int*, char*> d4;
-	Date<int&, char&> d5;
-		
-	
-	return 0;
+	funcA(1);
+	return 0;  
 }
