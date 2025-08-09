7554f28c83b81e7f4a97018a5da52e0e6ddf2023
Yiming <2018565194@qq.com>
2025-08-09 20:55:14 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,29 +2,29 @@
 #include<iostream>
 #include"FUNC.h"
 using namespace std;
-//#define N 10
-//template<class T>
-//class stack
-//{
-//private:
-//	T _a[N];
-//	int _top;
-//	int _capacity;
-//};
-//int main()
-//{
-//	stack<int> s1;//10
-//	stack<int> s2;//1000,单单使用宏N无法满足创建不同容量栈的需求
-//}
+#define N 10
+template<class T>
+class stack
+{
+private:
+	T _a[N];
+	int _top;
+	int _capacity;
+};
+int main()
+{
+	stack<int> s1;//10
+	stack<int> s2;//1000,单单使用宏N无法满足创建不同容量栈的需求
+}
 //使用非类型模版参数
-//template<class T=int,size_t N=100>
-//class stack2
-//{
-//private:
-//	T _a[N];
-//	int _top;
-//	int _capacity;
-//};
+template<class T=int,size_t N=100>
+class stack2
+{
+private:
+	T _a[N];
+	int _top;
+	int _capacity;
+};
 //int main()
 //{
 //	stack2<int,10> s1;//10
