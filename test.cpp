5a9bcbe2b1cd78746b386a162cf77d30bc2c8d10
Yiming <2018565194@qq.com>
2025-08-10 17:07:00 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,41 +2,41 @@
 #include<iostream>
 #include"FUNC.h"
 using namespace std;
-#define N 10
-template<class T>
-class stack
-{
-private:
-	T _a[N];
-	int _top;
-	int _capacity;
-};
-int main()
-{
-	stack<int> s1;//10
-	stack<int> s2;//1000,单单使用宏N无法满足创建不同容量栈的需求
-}
-//使用非类型模版参数
-template<class T=int,size_t N=100>
-class stack2
-{
-private:
-	T _a[N];
-	int _top;
-	int _capacity;
-};
+//#define N 10
+//template<class T>
+//class stack
+//{
+//private:
+//	T _a[N];
+//	int _top;
+//	int _capacity;
+//};
 //int main()
 //{
-//	stack2<int,10> s1;//10
-//	stack2<int,1000> s2;//1000
-//	stack2<int> s3;
-//	stack2<> s4;
-//	cout << sizeof(s1) << endl;
-//	cout << sizeof(s2) << endl;
-//	cout << sizeof(s3) << endl;
-//	cout << sizeof(s4) << endl;
-//	return 0; 
+//	stack<int> s1;//10
+//	stack<int> s2;//1000,单单使用宏N无法满足创建不同容量栈的需求
 //}
+////使用非类型模版参数
+//template<class T=int,size_t N=100>
+//class stack2
+//{
+//private:
+//	T _a[N];
+//	int _top;
+//	int _capacity;
+//};
+////int main()
+////{
+////	stack2<int,10> s1;//10
+////	stack2<int,1000> s2;//1000
+////	stack2<int> s3;
+////	stack2<> s4;
+////	cout << sizeof(s1) << endl;
+////	cout << sizeof(s2) << endl;
+////	cout << sizeof(s3) << endl;
+////	cout << sizeof(s4) << endl;
+////	return 0; 
+////}
 //template<class T>
 //bool Less(const T& left, const T& right)//使用&避免传值传参，同时const修饰避免使用引用将原值修改
 //{
